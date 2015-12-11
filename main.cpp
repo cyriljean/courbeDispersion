9efc00d26cc974eb2df1f380f424affee53154ab
cyriljean <cyril.jean@insp.upmc.fr>
2015-12-11 11:15:16 +0100

    commit initial du projet c++ de relations de dispersion

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "math.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a(2),b(2);
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+    b = ajouteDeux(a);                     //Appel de la fonction
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+    return 0;
+
+}
---
+++ math.cpp
@@ -0,0 +1,7 @@
+#include "math.h"
+
+int ajouteDeux(int nombreRecu)
+{
+    int valeur(nombreRecu + 2);
+    return valeur;
+}
